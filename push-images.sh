7034430157d10955b36f9cf97ec08ab1e3b1ae00
18623197617 <liuwenlongfox@163.com>
2020-02-27 12:52:47 +0800

    添加travis文件

---
+++ push-images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#push google or coreos images to dockerhub or aliyun registry,
+#change the registry name and username/password to yourself's.
+
+DOCKER_HUB=kevinniu666
+ALI_REGISTRY=registry.cn-hangzhou.aliyuncs.com/imagesfromgoogle
+
+#config the following env at travis-ui
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker login registry.cn-hangzhou.aliyuncs.com -u $ALI_USERNAME  -p $ALI_PASSWORD
+docker build -t "opentsdb:2.2.0" .
+docker push $DOCKER_HUB/opentsdb:2.2.0
