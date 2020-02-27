5117ec60d370146646c358527afee5aaaf45ee37
刘文龙 <liuwenlong@cloudwalk.cn>
2020-02-27 13:53:05 +0800

    fix docker push

--- push-images.sh
+++ push-images.sh
@@ -7,5 +7,5 @@ ALI_REGISTRY=registry.cn-hangzhou.aliyuncs.com/imagesfromgoogle
 echo "151.101.52.209 central.maven.org" >> /etc/hosts
 #config the following env at travis-ui
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build -t "opentsdb:2.2.0" .
+docker build -t "$DOCKER_HUB/opentsdb:2.2.0" .
 docker push $DOCKER_HUB/opentsdb:2.2.0
